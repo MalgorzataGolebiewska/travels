6ff094a856bdcbe77c8a9d66091e49d05d06cc90
MalgorzataGolebiewska <malgorzata.golebiewska01@gmail.com>
2023-04-03 16:33:22 +0200

    choosing a date using the calendar

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -1,6 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
@@ -21,8 +22,19 @@ public class HotelSearch {
         driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
         driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
         driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
-        driver.findElement(By.name("checkin")).sendKeys("17/04/2021");
-        driver.findElement(By.name("checkout")).sendKeys("18/04/2021");
+        driver.findElement(By.name("checkin")).click();
+        driver.findElements(By.xpath("//td[@class='day ' and text()='11']"))
+                .stream()
+                .filter(ell -> ell.isDisplayed())
+                .findFirst()
+                .ifPresent(ell -> ell.click());
+
+        driver.findElement(By.name("checkout")).click();
+        driver.findElements(By.xpath("//td[@class='day ' and text()='30']"))
+                .stream()
+                .filter(el -> el.isDisplayed())
+                .findFirst()
+                .ifPresent(el -> el.click());
 
     }
 }
